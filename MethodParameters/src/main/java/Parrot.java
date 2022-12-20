a86ed4d465500aa887e7d70f4867b80c8913600f
Kristina Kurganskaia <kurganskaiakristina@gmail.com>
2022-12-20 17:08:15 +0000

    Saved progress on MethodParameters 2022-12-20 17:08:15 +0000

--- MethodParameters/src/main/java/Parrot.java
+++ MethodParameters/src/main/java/Parrot.java
@@ -11,7 +11,10 @@ public class Parrot {
      * @param text is a parameter passed into this method by whatever called it.
      * @return text.
      */
+    String one = "hello";
+    String two = "polly";
     public String copy(String text){
-        return "return text please!";
+        return text;
+      
     }
 }
